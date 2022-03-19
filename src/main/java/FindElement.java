e9376d945827e5c30953593dcce914e2e5e6dff9
Citkacy <michalowski-marcin@wp.pl>
2022-03-19 22:24:18 +0100

    Update files

--- src/main/java/FindElement.java
+++ src/main/java/FindElement.java
@@ -5,34 +5,43 @@ import java.util.List;
 
 public class FindElement {
 
+    //invalid login message
+    @FindBy(id = "spanMessage")
+    WebElement spanMessage;
+
+    //textbox
     @FindBy(id = "txtUsername")
     WebElement inputLogin;
     @FindBy(css = "input[id='txtPassword']")
     WebElement inputPassword;
-    @FindBy(xpath = "//*[@id=\"menu_admin_viewAdminModule\"]/b")
-    WebElement adminModule;
-    @FindBy(id = "menu_pim_viewPimModule")
-    WebElement pimModule;
-    @FindBy(id = "btnAdd")
-    WebElement addEmployee;
     @FindBy(id = "firstName")
     WebElement inputFirstName;
     @FindBy(id = "lastName")
     WebElement inputLastName;
-    @FindBy(id = "personal_txtEmpFirstName")
-    WebElement txtEmpFirstName;
-    @FindBy(id = "personal_txtEmpLastName")
-    WebElement txtEmpLastName;
+    @FindBy(className = "firstLevelMenu")
+    List<WebElement> mainMenu;
+
+    //menu
+    @FindBy(xpath = "//*[@id=\"menu_admin_viewAdminModule\"]/b")
+    WebElement adminModule;
+    @FindBy(id = "menu_pim_viewPimModule")
+    WebElement pimModule;
     @FindBy(xpath = "//*[@id=\"menu_admin_Configuration\"]")
     WebElement menu;
     @FindBy(id = "menu_admin_localization")
     WebElement localization;
-    @FindBy(id = "btnSave")
-    WebElement button;
     @FindBy(id = "localization_dafault_language")
     WebElement languageBar;
-    @FindBy(className = "firstLevelMenu")
-    List<WebElement> mainMenu;
-    @FindBy(id = "spanMessage")
-    WebElement spanMessage;
+
+    //button
+    @FindBy(id = "btnAdd")
+    WebElement addEmployee;
+    @FindBy(id = "btnSave")
+    WebElement button;
+
+    //get text
+    @FindBy(id = "personal_txtEmpFirstName")
+    WebElement txtEmpFirstName;
+    @FindBy(id = "personal_txtEmpLastName")
+    WebElement txtEmpLastName;
 }
