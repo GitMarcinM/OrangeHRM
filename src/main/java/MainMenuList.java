20e5760e66eccdcf28967d13c9e72ac3842c4547
Marcin <95912851+GitMarcinM@users.noreply.github.com>
2022-03-21 16:27:46 +0100

    Update MainMenuList.java
    Removing excess code

--- src/main/java/MainMenuList.java
+++ src/main/java/MainMenuList.java
@@ -3,8 +3,5 @@ import java.util.List;
 
 public class MainMenuList {
 
-    public static final List<String> targetMenu = Arrays.asList("Admin", "Personendatenmanagement", "Abwesenheiten", "Zeit", "Bewerbermanagement", "Persönliche Informationen", "Performance", "Cockpit", "Directory", "Maintenance", "Buzz");
-//    public static final List<String> targetMenu = Arrays.asList("Admin", "Personendatenmanagement", "Abwesenheiten", "Zeit", "Bewerbermanagement", "Persönliche Informationen", "Leistung", "Cockpit", "Verzeichnis", "Wartung", "Buzz");
-
-
+    public static final List<String> targetMenu = Arrays.asList("Admin", "Personendatenmanagement", "Abwesenheiten", "Zeit", "Bewerbermanagement", "Persönliche Informationen", "Leistung", "Cockpit", "Verzeichnis", "Wartung", "Buzz");
 }
